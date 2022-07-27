83ca3065788ed0a221eb90c547f63f905cb905bf
denysmarakhovskyi <denysmarakhovskyi@gmail.com>
2022-07-27 14:25:19 +0200

    L5

---
+++ popularity.py
@@ -0,0 +1,11 @@
+import sys
+
+
+def popularity(text):
+    text = popularity
+    return mass
+
+popularity('apple kiwi pineapple kiwi apple kiwi')
+popularity('aPPle pine Apple kiwi Apple kiwi')
+popularity('aPPle pine Apple kiwi Apple kiwi')
+popularity('aab aaa aac aab aac aaa x')
